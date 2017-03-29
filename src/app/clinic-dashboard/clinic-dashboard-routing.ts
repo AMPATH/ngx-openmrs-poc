enyachoke/ng2-amrs

NGPOC-208:Add the daily schedule tab features

src/app/clinic-dashboard/clinic-dashboard-routing.ts
@@ -6,6 +6,13 @@ import { ClinicDashboardComponent } from './clinic-dashboard.component';
 import { ClinicDashboardGuard } from './clinic-dashboard.guard';
 import { MonthlyScheduleComponent } from './monthly-schedule/monthly-schedule.component';
 import { VisualizationComponent } from './clinical-summary-visualization/visualization-component';
+
+import { DailyScheduleVisitsComponent } from './daily-schedule/daily-schedule-visits.component';
+import { DailyScheduleAppointmentsComponent }
+  from './daily-schedule/daily-schedule-appointments.component';
+import { DailyScheduleNotReturned } from './daily-schedule/daily-schedule-not-returned.component';
+
+
 const clinicDashboardRoutes: Routes = [
   {
     path: '', component: ClinicDashboardComponent,
@@ -17,13 +24,20 @@ const clinicDashboardRoutes: Routes = [
     path: ':location_uuid', component: ClinicDashboardComponent,
     children: [
       {
-        path: 'daily-schedule', component: DailyScheduleComponent
+        path: 'daily-schedule', component: DailyScheduleComponent,
+        children: [
+          { path: 'daily-visits', component: DailyScheduleVisitsComponent },
+          { path: 'daily-appointments', component: DailyScheduleAppointmentsComponent },
+          { path: 'daily-not-returned', component: DailyScheduleNotReturned },
+          { path: '', redirectTo: 'daily-appointments', pathMatch: 'prefix' },
+        ]
+
       },
       { path: 'monthly-schedule', component: MonthlyScheduleComponent },
       {
         path: 'visualization', component: VisualizationComponent
       },
-      { path: '', redirectTo: 'daily-schedule', pathMatch: 'prefix' },
+      { path: '', redirectTo: 'daily-schedulexxx' + [0], pathMatch: 'prefix' },
     ],
     canActivate: [
       ClinicDashboardGuard

src/app/clinic-dashboard/daily-schedule/daily-schedule-not-returned.component.spec.ts
@@ -1,0 +1,132 @@
+/* tslint:disable:no-unused-variable */
+
+import { TestBed, async } from '@angular/core/testing';
+import { FormsModule } from '@angular/forms';
+import { Http, BaseRequestOptions } from '@angular/http';
+import { MockBackend } from '@angular/http/testing';
+import { DataListsModule } from '../../data-lists/data-lists.module';
+import { ClinicDashboardCacheService } from '../services/clinic-dashboard-cache.service';
+import {
+  DailyScheduleResourceService
+} from
+  '../../etl-api/daily-scheduled-resource.service';
+import { DailyScheduleNotReturned } from './daily-schedule-not-returned.component';
+import { AppFeatureAnalytics } from '../../shared/app-analytics/app-feature-analytics.service';
+import { FakeAppFeatureAnalytics } from '../../shared/app-analytics/app-feature-analytcis.mock';
+import { AppSettingsService } from '../../app-settings/app-settings.service';
+import { LocalStorageService } from '../../utils/local-storage.service';
+import { BusyModule, BusyConfig } from 'angular2-busy';
+import {
+  AccordionModule, DataTableModule, SharedModule, TabViewModule,
+  GrowlModule, PanelModule, ConfirmDialogModule, ConfirmationService,
+  DialogModule, InputTextModule, MessagesModule, InputTextareaModule,
+  DropdownModule, ButtonModule, CalendarModule
+} from 'primeng/primeng';
+import { Router, ActivatedRoute } from '@angular/router';
+import { CacheService } from 'ionic-cache/ionic-cache';
+import { DataCacheService } from '../../shared/services/data-cache.service';
+import { NgamrsSharedModule } from '../../shared/ngamrs-shared.module';
+describe('Component: DailyScheduleNotReturned', () => {
+  let fakeAppFeatureAnalytics: AppFeatureAnalytics, component,
+    dailyScheduleResource: DailyScheduleResourceService,
+    clinicDashBoardCacheService: ClinicDashboardCacheService,
+    fixture, componentInstance;
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      providers: [
+        LocalStorageService,
+        DailyScheduleResourceService,
+        ClinicDashboardCacheService,
+        MockBackend,
+        BaseRequestOptions,
+        AppSettingsService,
+        LocalStorageService,
+        CacheService,
+        DataCacheService,
+        {
+          provide: Router,
+          useClass: class { navigate = jasmine.createSpy('navigate'); }
+        },
+        {
+          provide: Http,
+          useFactory: (backendInstance: MockBackend,
+            defaultOptions: BaseRequestOptions) => {
+            return new Http(backendInstance, defaultOptions);
+          },
+          deps: [MockBackend, BaseRequestOptions]
+        },
+        {
+          provide: AppFeatureAnalytics, useFactory: () => {
+            return new FakeAppFeatureAnalytics();
+          }, deps: []
+        }
+
+      ],
+      declarations: [DailyScheduleNotReturned],
+      imports: [BusyModule,
+        FormsModule,
+        DialogModule,
+        CalendarModule,
+        DataListsModule,
+        NgamrsSharedModule]
+    });
+  });
+
+  beforeEach(async(() => {
+    TestBed.compileComponents().then(() => {
+      fixture = TestBed.createComponent(DailyScheduleNotReturned);
+      component = fixture.componentInstance;
+    });
+  }));
+
+  afterEach(() => {
+    TestBed.resetTestingModule();
+  });
+
+  it('should create an instance', () => {
+    clinicDashBoardCacheService = TestBed.get(ClinicDashboardCacheService);
+    dailyScheduleResource = TestBed.get(DailyScheduleResourceService);
+    let appointmentsComponent = new DailyScheduleNotReturned(clinicDashBoardCacheService,
+      dailyScheduleResource);
+    expect(appointmentsComponent).toBeTruthy();
+  });
+
+  it('should have required properties', (done) => {
+
+    expect(component.notReturnedPatientList.length).toBe(0);
+    expect(component.selectedDate).toBeDefined;
+    expect(component.ngOnInit).toBeDefined();
+    expect(component.ngOnChanges).toBeDefined();
+    expect(component.getQueryParams).toBeDefined();
+    expect(component.getDailyHasNotReturned).toBeDefined();
+    expect(component.errors.length).toBe(0);
+    expect(component.selectedDate).toEqual(undefined);
+    expect(component.selectedClinic).toEqual(undefined);
+    expect(component.dataLoaded).toEqual(false);
+    expect(component.loadingDailyNotReturned).toEqual(false);
+
+    done();
+
+  });
+
+  it('should call component methods', (done) => {
+    clinicDashBoardCacheService = TestBed.get(ClinicDashboardCacheService);
+    component.selectedDate = '12-12-2016';
+    spyOn(component, 'getDailyHasNotReturned').and.callThrough();
+    component.getDailyHasNotReturned({
+      startDate: '2017-02-01',
+      startIndex: undefined,
+      locationUuids: 'uuid',
+      limit: undefined
+    });
+    expect(component.getDailyHasNotReturned).toHaveBeenCalled();
+    spyOn(component, 'ngOnInit').and.callThrough();
+    component.ngOnInit();
+    expect(component.ngOnInit).toHaveBeenCalled();
+
+    done();
+  });
+
+});
+
+

src/app/clinic-dashboard/daily-schedule/daily-schedule-visits.component.spec.ts
@@ -1,0 +1,131 @@
+/* tslint:disable:no-unused-variable */
+
+import { TestBed, async } from '@angular/core/testing';
+import { FormsModule } from '@angular/forms';
+import { Http, BaseRequestOptions } from '@angular/http';
+import { MockBackend } from '@angular/http/testing';
+import { DataListsModule } from '../../data-lists/data-lists.module';
+import { ClinicDashboardCacheService } from '../services/clinic-dashboard-cache.service';
+import {
+  DailyScheduleResourceService
+} from
+  '../../etl-api/daily-scheduled-resource.service';
+import { DailyScheduleVisitsComponent } from './daily-schedule-visits.component';
+import { AppFeatureAnalytics } from '../../shared/app-analytics/app-feature-analytics.service';
+import { FakeAppFeatureAnalytics } from '../../shared/app-analytics/app-feature-analytcis.mock';
+import { AppSettingsService } from '../../app-settings/app-settings.service';
+import { LocalStorageService } from '../../utils/local-storage.service';
+import { BusyModule, BusyConfig } from 'angular2-busy';
+import {
+  AccordionModule, DataTableModule, SharedModule, TabViewModule,
+  GrowlModule, PanelModule, ConfirmDialogModule, ConfirmationService,
+  DialogModule, InputTextModule, MessagesModule, InputTextareaModule,
+  DropdownModule, ButtonModule, CalendarModule
+} from 'primeng/primeng';
+import { Router } from '@angular/router';
+import { CacheService } from 'ionic-cache/ionic-cache';
+import { DataCacheService } from '../../shared/services/data-cache.service';
+import { NgamrsSharedModule } from '../../shared/ngamrs-shared.module';
+describe('Component: DailyScheduleVisitsComponent', () => {
+  let fakeAppFeatureAnalytics: AppFeatureAnalytics, component,
+    dailyScheduleResource: DailyScheduleResourceService,
+    clinicDashBoardCacheService: ClinicDashboardCacheService,
+    fixture, componentInstance;
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      providers: [
+        LocalStorageService,
+        DailyScheduleResourceService,
+        ClinicDashboardCacheService,
+        MockBackend,
+        BaseRequestOptions,
+        AppSettingsService,
+        LocalStorageService,
+        CacheService,
+        DataCacheService,
+        {
+          provide: Router,
+          useClass: class { navigate = jasmine.createSpy('navigate'); }
+        },
+        {
+          provide: Http,
+          useFactory: (backendInstance: MockBackend,
+            defaultOptions: BaseRequestOptions) => {
+            return new Http(backendInstance, defaultOptions);
+          },
+          deps: [MockBackend, BaseRequestOptions]
+        },
+        {
+          provide: AppFeatureAnalytics, useFactory: () => {
+            return new FakeAppFeatureAnalytics();
+          }, deps: []
+        }
+
+      ],
+      declarations: [DailyScheduleVisitsComponent],
+      imports: [BusyModule,
+        FormsModule,
+        DialogModule,
+        CalendarModule,
+        DataListsModule, NgamrsSharedModule]
+    });
+  });
+
+  beforeEach(async(() => {
+    TestBed.compileComponents().then(() => {
+      fixture = TestBed.createComponent(DailyScheduleVisitsComponent);
+      component = fixture.componentInstance;
+    });
+  }));
+
+  afterEach(() => {
+    TestBed.resetTestingModule();
+  });
+
+  it('should create an instance', () => {
+    clinicDashBoardCacheService = TestBed.get(ClinicDashboardCacheService);
+    dailyScheduleResource = TestBed.get(DailyScheduleResourceService);
+    let appointmentsComponent = new DailyScheduleVisitsComponent(clinicDashBoardCacheService,
+      dailyScheduleResource);
+    expect(appointmentsComponent).toBeTruthy();
+  });
+
+  it('should have required properties', (done) => {
+
+    expect(component.dailyVisitsPatientList.length).toBe(0);
+    expect(component.selectedDate).toBeDefined;
+    expect(component.ngOnInit).toBeDefined();
+    expect(component.ngOnChanges).toBeDefined();
+    expect(component.getQueryParams).toBeDefined();
+    expect(component.getDailyVisits).toBeDefined();
+    expect(component.errors.length).toBe(0);
+    expect(component.selectedDate).toEqual(undefined);
+    expect(component.selectedClinic).toEqual(undefined);
+    expect(component.dataLoaded).toEqual(false);
+    expect(component.loadingDailyVisits).toEqual(false);
+
+    done();
+
+  });
+
+  it('should call component methods', (done) => {
+    clinicDashBoardCacheService = TestBed.get(ClinicDashboardCacheService);
+    component.selectedDate = '12-12-2016';
+    spyOn(component, 'getDailyVisits').and.callThrough();
+    component.getDailyVisits({
+      startDate: '2017-02-01',
+      startIndex: undefined,
+      locationUuids: 'uuid',
+      limit: undefined
+    });
+    expect(component.getDailyVisits).toHaveBeenCalled();
+    spyOn(component, 'ngOnInit').and.callThrough();
+    component.ngOnInit();
+    expect(component.ngOnInit).toHaveBeenCalled();
+
+    done();
+  });
+
+});
+
+

AMPATH/ngx-openmrs-poc

src/app/clinic-dashboard/clinic-dashboard-routing.ts
@@ -7,6 +7,10 @@ import { ClinicDashboardGuard } from './clinic-dashboard.guard';
 import { MonthlyScheduleComponent } from './monthly-schedule/monthly-schedule.component';
 import { VisualizationComponent } from './clinical-summary-visualization/visualization-component';
 import { ClinicLabOrdersComponent } from './clinic-lab-orders/clinic-lab-orders.component';
+import { DailyScheduleVisitsComponent } from './daily-schedule/daily-schedule-visits.component';
+import { DailyScheduleAppointmentsComponent }
+  from './daily-schedule/daily-schedule-appointments.component';
+import { DailyScheduleNotReturned } from './daily-schedule/daily-schedule-not-returned.component';
 const clinicDashboardRoutes: Routes = [
   {
     path: '', component: ClinicDashboardComponent,
@@ -18,14 +22,21 @@ const clinicDashboardRoutes: Routes = [
     path: ':location_uuid', component: ClinicDashboardComponent,
     children: [
       {
-        path: 'daily-schedule', component: DailyScheduleComponent
+        path: 'daily-schedule', component: DailyScheduleComponent,
+        children: [
+          { path: 'daily-visits', component: DailyScheduleVisitsComponent },
+          { path: 'daily-appointments', component: DailyScheduleAppointmentsComponent },
+          { path: 'daily-not-returned', component: DailyScheduleNotReturned },
+          { path: '', redirectTo: 'daily-appointments', pathMatch: 'prefix' },
+        ]
+
       },
       { path: 'monthly-schedule', component: MonthlyScheduleComponent },
       {
         path: 'visualization', component: VisualizationComponent
       },
       { path: 'clinic-lab-orders', component: ClinicLabOrdersComponent },
-      { path: '', redirectTo: 'daily-schedule', pathMatch: 'prefix' },
+      { path: '', redirectTo: 'daily-schedule' + [0], pathMatch: 'prefix' },
     ],
     canActivate: [
       ClinicDashboardGuard

src/app/clinic-dashboard/daily-schedule/daily-schedule-not-returned.component.spec.ts
@@ -1,0 +1,144 @@
+/* tslint:disable:no-unused-variable */
+
+import { TestBed, async } from '@angular/core/testing';
+import { FormsModule } from '@angular/forms';
+import { Http, BaseRequestOptions } from '@angular/http';
+import { MockBackend } from '@angular/http/testing';
+import { DataListsModule } from '../../data-lists/data-lists.module';
+import { ClinicDashboardCacheService } from '../services/clinic-dashboard-cache.service';
+import {
+  DailyScheduleResourceService
+} from
+  '../../etl-api/daily-scheduled-resource.service';
+import { DailyScheduleNotReturned } from './daily-schedule-not-returned.component';
+import { AppFeatureAnalytics } from '../../shared/app-analytics/app-feature-analytics.service';
+import { FakeAppFeatureAnalytics } from '../../shared/app-analytics/app-feature-analytcis.mock';
+import { AppSettingsService } from '../../app-settings/app-settings.service';
+import { LocalStorageService } from '../../utils/local-storage.service';
+import { BusyModule, BusyConfig } from 'angular2-busy';
+import {
+  AccordionModule, DataTableModule, SharedModule, TabViewModule,
+  GrowlModule, PanelModule, ConfirmDialogModule, ConfirmationService,
+  DialogModule, InputTextModule, MessagesModule, InputTextareaModule,
+  DropdownModule, ButtonModule, CalendarModule
+} from 'primeng/primeng';
+import { Router, ActivatedRoute } from '@angular/router';
+import { CacheService } from 'ionic-cache/ionic-cache';
+import { DataCacheService } from '../../shared/services/data-cache.service';
+import { NgamrsSharedModule } from '../../shared/ngamrs-shared.module';
+describe('Component: DailyScheduleNotReturned', () => {
+  let fakeAppFeatureAnalytics: AppFeatureAnalytics, component,
+    dailyScheduleResource: DailyScheduleResourceService,
+    clinicDashBoardCacheService: ClinicDashboardCacheService,
+    fixture, componentInstance;
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      providers: [
+        LocalStorageService,
+        DailyScheduleResourceService,
+        ClinicDashboardCacheService,
+        MockBackend,
+        BaseRequestOptions,
+        AppSettingsService,
+        LocalStorageService,
+        CacheService,
+        DataCacheService,
+        {
+          provide: Router,
+          useClass: class { navigate = jasmine.createSpy('navigate'); }
+        },
+        {
+          provide: Http,
+          useFactory: (backendInstance: MockBackend,
+            defaultOptions: BaseRequestOptions) => {
+            return new Http(backendInstance, defaultOptions);
+          },
+          deps: [MockBackend, BaseRequestOptions]
+        },
+        {
+          provide: AppFeatureAnalytics, useFactory: () => {
+            return new FakeAppFeatureAnalytics();
+          }, deps: []
+        }
+
+      ],
+      declarations: [DailyScheduleNotReturned],
+      imports: [BusyModule,
+        FormsModule,
+        DialogModule,
+        CalendarModule,
+        DataListsModule,
+        NgamrsSharedModule]
+    });
+  });
+
+  beforeEach(async(() => {
+    TestBed.compileComponents().then(() => {
+      fixture = TestBed.createComponent(DailyScheduleNotReturned);
+      component = fixture.componentInstance;
+    });
+  }));
+
+  afterEach(() => {
+    TestBed.resetTestingModule();
+  });
+
+  it('should create an instance', () => {
+    clinicDashBoardCacheService = TestBed.get(ClinicDashboardCacheService);
+    dailyScheduleResource = TestBed.get(DailyScheduleResourceService);
+    let appointmentsComponent = new DailyScheduleNotReturned(clinicDashBoardCacheService,
+      dailyScheduleResource);
+    expect(appointmentsComponent).toBeTruthy();
+  });
+
+  it('should have required properties', (done) => {
+
+    expect(component.notReturnedPatientList.length).toBe(0);
+    expect(component.selectedDate).toBeDefined;
+    expect(component.ngOnInit).toBeDefined();
+    expect(component.getQueryParams).toBeDefined();
+    expect(component.getDailyHasNotReturned).toBeDefined();
+    expect(component.errors.length).toBe(0);
+    expect(component.selectedDate).toEqual(undefined);
+    expect(component.selectedClinic).toEqual(undefined);
+    expect(component.dataLoaded).toEqual(false);
+    expect(component.loadingDailyNotReturned).toEqual(false);
+
+    done();
+
+  });
+
+  it('should call component methods', (done) => {
+    clinicDashBoardCacheService = TestBed.get(ClinicDashboardCacheService);
+    component.selectedDate = '12-12-2016';
+    spyOn(component, 'getDailyHasNotReturned').and.callThrough();
+    component.getDailyHasNotReturned({
+      startDate: '2017-02-01',
+      startIndex: undefined,
+      locationUuids: 'uuid',
+      limit: undefined
+    });
+    expect(component.getDailyHasNotReturned).toHaveBeenCalled();
+    spyOn(component, 'ngOnInit').and.callThrough();
+    component.ngOnInit();
+    expect(component.ngOnInit).toHaveBeenCalled();
+
+    done();
+  });
+
+  it('should create params when getQueryParams is called', (done) => {
+    spyOn(component, 'getQueryParams').and.callThrough();
+    component.selectedDate = '12-12-2016';
+    component.selectedClinic = 'location-uuid';
+    let params = component.getQueryParams();
+    expect(component.getQueryParams).toHaveBeenCalled();
+    expect(params.locationUuids).toEqual('location-uuid');
+    expect(params.startDate).toEqual('12-12-2016');
+    expect(params.limit).toBeUndefined;
+    expect(params.startIndex).toEqual(0);
+    done();
+  });
+
+});
+
+

src/app/clinic-dashboard/daily-schedule/daily-schedule-visits.component.spec.ts
@@ -1,0 +1,143 @@
+/* tslint:disable:no-unused-variable */
+
+import { TestBed, async } from '@angular/core/testing';
+import { FormsModule } from '@angular/forms';
+import { Http, BaseRequestOptions } from '@angular/http';
+import { MockBackend } from '@angular/http/testing';
+import { DataListsModule } from '../../data-lists/data-lists.module';
+import { ClinicDashboardCacheService } from '../services/clinic-dashboard-cache.service';
+import {
+  DailyScheduleResourceService
+} from
+  '../../etl-api/daily-scheduled-resource.service';
+import { DailyScheduleVisitsComponent } from './daily-schedule-visits.component';
+import { AppFeatureAnalytics } from '../../shared/app-analytics/app-feature-analytics.service';
+import { FakeAppFeatureAnalytics } from '../../shared/app-analytics/app-feature-analytcis.mock';
+import { AppSettingsService } from '../../app-settings/app-settings.service';
+import { LocalStorageService } from '../../utils/local-storage.service';
+import { BusyModule, BusyConfig } from 'angular2-busy';
+import {
+  AccordionModule, DataTableModule, SharedModule, TabViewModule,
+  GrowlModule, PanelModule, ConfirmDialogModule, ConfirmationService,
+  DialogModule, InputTextModule, MessagesModule, InputTextareaModule,
+  DropdownModule, ButtonModule, CalendarModule
+} from 'primeng/primeng';
+import { Router } from '@angular/router';
+import { CacheService } from 'ionic-cache/ionic-cache';
+import { DataCacheService } from '../../shared/services/data-cache.service';
+import { NgamrsSharedModule } from '../../shared/ngamrs-shared.module';
+describe('Component: DailyScheduleVisitsComponent', () => {
+  let fakeAppFeatureAnalytics: AppFeatureAnalytics, component,
+    dailyScheduleResource: DailyScheduleResourceService,
+    clinicDashBoardCacheService: ClinicDashboardCacheService,
+    fixture, componentInstance;
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      providers: [
+        LocalStorageService,
+        DailyScheduleResourceService,
+        ClinicDashboardCacheService,
+        MockBackend,
+        BaseRequestOptions,
+        AppSettingsService,
+        LocalStorageService,
+        CacheService,
+        DataCacheService,
+        {
+          provide: Router,
+          useClass: class { navigate = jasmine.createSpy('navigate'); }
+        },
+        {
+          provide: Http,
+          useFactory: (backendInstance: MockBackend,
+            defaultOptions: BaseRequestOptions) => {
+            return new Http(backendInstance, defaultOptions);
+          },
+          deps: [MockBackend, BaseRequestOptions]
+        },
+        {
+          provide: AppFeatureAnalytics, useFactory: () => {
+            return new FakeAppFeatureAnalytics();
+          }, deps: []
+        }
+
+      ],
+      declarations: [DailyScheduleVisitsComponent],
+      imports: [BusyModule,
+        FormsModule,
+        DialogModule,
+        CalendarModule,
+        DataListsModule, NgamrsSharedModule]
+    });
+  });
+
+  beforeEach(async(() => {
+    TestBed.compileComponents().then(() => {
+      fixture = TestBed.createComponent(DailyScheduleVisitsComponent);
+      component = fixture.componentInstance;
+    });
+  }));
+
+  afterEach(() => {
+    TestBed.resetTestingModule();
+  });
+
+  it('should create an instance', () => {
+    clinicDashBoardCacheService = TestBed.get(ClinicDashboardCacheService);
+    dailyScheduleResource = TestBed.get(DailyScheduleResourceService);
+    let appointmentsComponent = new DailyScheduleVisitsComponent(clinicDashBoardCacheService,
+      dailyScheduleResource);
+    expect(appointmentsComponent).toBeTruthy();
+  });
+
+  it('should have required properties', (done) => {
+
+    expect(component.dailyVisitsPatientList.length).toBe(0);
+    expect(component.selectedDate).toBeDefined;
+    expect(component.ngOnInit).toBeDefined();
+    expect(component.getQueryParams).toBeDefined();
+    expect(component.getDailyVisits).toBeDefined();
+    expect(component.errors.length).toBe(0);
+    expect(component.selectedDate).toEqual(undefined);
+    expect(component.selectedClinic).toEqual(undefined);
+    expect(component.dataLoaded).toEqual(false);
+    expect(component.loadingDailyVisits).toEqual(false);
+
+    done();
+
+  });
+
+  it('should call component methods', (done) => {
+    clinicDashBoardCacheService = TestBed.get(ClinicDashboardCacheService);
+    component.selectedDate = '12-12-2016';
+    spyOn(component, 'getDailyVisits').and.callThrough();
+    component.getDailyVisits({
+      startDate: '2017-02-01',
+      startIndex: undefined,
+      locationUuids: 'uuid',
+      limit: undefined
+    });
+    expect(component.getDailyVisits).toHaveBeenCalled();
+    spyOn(component, 'ngOnInit').and.callThrough();
+    component.ngOnInit();
+    expect(component.ngOnInit).toHaveBeenCalled();
+
+    done();
+  });
+
+  it('should create params when getQueryParams is called', (done) => {
+    spyOn(component, 'getQueryParams').and.callThrough();
+    component.selectedDate = '12-12-2016';
+    component.selectedClinic = 'location-uuid';
+    let params = component.getQueryParams();
+    expect(component.getQueryParams).toHaveBeenCalled();
+    expect(params.locationUuids).toEqual('location-uuid');
+    expect(params.startDate).toEqual('12-12-2016');
+    expect(params.limit).toBeUndefined;
+    expect(params.startIndex).toEqual(0);
+    done();
+  });
+
+});
+
+